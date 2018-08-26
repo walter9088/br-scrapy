1c735fa6fc9ff95b1aec242761c7de5e520723b8
walter9088 <515771829@qq.com>
2018-08-27 02:18:12 +0800

    update

---
+++ bRGovScrapy/spiders/NcGovPurchase_spider.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import urllib2
+import jieba.analyse
+import datetime
+from datetime import timedelta
+
+
+from bs4 import BeautifulSoup
+
+#import html5lib
+
+
+jieba.load_userdict('data/dict.txt')
+jieba.analyse.set_stop_words('data/stopword.txt')
+
+date = datetime.date.today()
+yesterday = date + timedelta(days = -1)
+
+yesterday_str = str(yesterday)
+
+class NcGovPurchase(scrapy.Spider):
+    name = "nc_gov_purchase"
+    allowed_domains = ["ncszfcg.gov.cn"]
+    start_urls = ["http://www.ncszfcg.gov.cn/more2018.cfm?sid=100002003&c_code=&area="]
+
+
+    def parse(self,response):
+
+        dome_url = 'http://www.ncszfcg.gov.cn/'
+        data = response.body
+
+        soup = BeautifulSoup(data)
+
+
+        list_box = soup.find_all("ul", class_='listbox')
+
+        a_list = list_box[0].find_all('a')
+
+        yesterday_str = '2018-03-16'
+
+        for a in a_list:
+
+            #每天爬取前一天数据，对日期做一个限色
+            date_web = a.find_next("div",class_='date').string
+
+            if date_web == yesterday_str:
+
+                URL = dome_url +a['href']
+
+                page = urllib2.urlopen(URL)
+
+                d_soup = BeautifulSoup(page)
+
+                qd_tables = d_soup.find_all("table", class_="MsoNormalTable")
+
+                if len(qd_tables)>=2:
+                    qd_table = qd_tables[1]
+                elif len(qd_tables)==1:
+                    qd_table = qd_tables[0]
+                else:
+                    continue
+
+                key = jieba.analyse.extract_tags(qd_table.getText(), topK=50, withFlag=False, allowPOS=('n', 'vn', 'v', 'nbr'))
+
+
+
+
+
+        # str = qd_table.getText()
+        # key = jieba.analyse.extract_tags(str, topK=50, withFlag=False, allowPOS=('n', 'vn', 'v', 'nbr'))
